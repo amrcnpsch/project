1317128e46ef0cb92d3e100b697b15ebbec0e67e
literally me <117268581+amrcnpsch@users.noreply.github.com>
2023-06-22 17:56:27 +0300

    Update nums.py
    I added delenie

--- Learning/nums.py
+++ Learning/nums.py
@@ -2,5 +2,10 @@ def plus(a,b):
     return a+b
 def minus(a,b):
     return a-b
+def delenie(a,b):
+    if b != 0:
+        return a/b
+    else:
+        print("B is a null!")
 text = "text"
 print(minus(int(input()),int(input())))
